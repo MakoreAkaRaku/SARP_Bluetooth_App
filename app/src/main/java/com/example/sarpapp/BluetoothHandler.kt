2759c7c256b41e541aca33568a2647c6bac50197
MakoreAkaRaku <marcromancolom@gmail.com>
2024-10-22 12:27:31 +0200

    started implement broadcastMsg for the bleAdvertiser. Keeps pending building the settings plus the message

--- app/src/main/java/com/example/sarpapp/BluetoothHandler.kt
+++ app/src/main/java/com/example/sarpapp/BluetoothHandler.kt
@@ -2,8 +2,10 @@ package com.example.sarpapp
 
 import android.Manifest
 import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothClass.Device
 import android.bluetooth.BluetoothManager
+import android.bluetooth.le.AdvertiseCallback
+import android.bluetooth.le.AdvertiseData
+import android.bluetooth.le.AdvertiseSettings
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
 import android.bluetooth.le.ScanSettings
@@ -12,19 +14,32 @@ import android.os.Build.VERSION_CODES.JELLY_BEAN_MR1
 import android.os.Handler
 import android.os.Looper
 import androidx.annotation.RequiresPermission
-import java.util.concurrent.Executor
-import java.util.concurrent.ExecutorService
-
 
 class BluetoothHandler(
     private val btManager: BluetoothManager,
-) : ScanCallback() {
+){
 
     private var bluetoothAdapter: BluetoothAdapter
     private final val REQUEST_PERMISSION_PHONE_STATE = 1
     private var isScanning = false
+    private var isAdvertising = false
+    private val BROADCAST_TIME : Long = 5000
     private val handler: Handler = Handler(Looper.getMainLooper())
+    private val leAdvertiserCallback = object: AdvertiseCallback(){
 
+    }
+
+    private val leScanCallback = object: ScanCallback(){
+        override fun onScanResult(callbackType: Int, result: ScanResult?) {
+            when(callbackType) {
+                ScanSettings.CALLBACK_TYPE_ALL_MATCHES -> println("Type All Matches")
+
+                ScanSettings.CALLBACK_TYPE_FIRST_MATCH -> println("Type First Match")
+                ScanSettings.CALLBACK_TYPE_FIRST_MATCH -> println("Type First Match")
+                else -> println("I was not expecting anything else...")
+            }
+        }
+    }
     init {
         bluetoothAdapter = if (Build.VERSION.SDK_INT in 1..JELLY_BEAN_MR1)
             BluetoothAdapter.getDefaultAdapter()
@@ -37,11 +52,11 @@ class BluetoothHandler(
         if (!isScanning)
         {
             isScanning = true
-            bluetoothAdapter.bluetoothLeScanner.startScan(this)
+            bluetoothAdapter.bluetoothLeScanner.startScan(leScanCallback)
             handler.postDelayed(
                 {
                     isScanning = false
-                    bluetoothAdapter.bluetoothLeScanner.stopScan(this)
+                    bluetoothAdapter.bluetoothLeScanner.stopScan(leScanCallback)
                 },
                 timeToScanMs
             )
@@ -50,19 +65,24 @@ class BluetoothHandler(
         return false
     }
 
-    override fun onScanResult(callbackType: Int, result: ScanResult?) {
-        when(callbackType) {
-            ScanSettings.CALLBACK_TYPE_ALL_MATCHES -> println("Type All Matches")
-
-            ScanSettings.CALLBACK_TYPE_FIRST_MATCH -> println("Type First Match")
-            ScanSettings.CALLBACK_TYPE_FIRST_MATCH -> println("Type First Match")
-            else -> println("I was not expecting anything else...")
+    @RequiresPermission(Manifest.permission.BLUETOOTH_ADVERTISE)
+    fun broadcastMsg(ssid : String,pwd: String): Boolean {
+        if (!isAdvertising)
+        {
+            var advertiseMsg : AdvertiseData.Builder = AdvertiseData.Builder()
+            var advertiseSettings : AdvertiseSettings.Builder = AdvertiseSettings.Builder()
+            //TODO keep building the message plus the settings.
+            isAdvertising = true
+            handler.postDelayed(
+                {
+                    isAdvertising = false
+                    bluetoothAdapter.bluetoothLeAdvertiser.stopAdvertising(leAdvertiserCallback)
+                },
+                BROADCAST_TIME
+            )
+            bluetoothAdapter.bluetoothLeAdvertiser.startAdvertising(advertiseSettings.build(),advertiseMsg.build(),leAdvertiserCallback)
+            return true
         }
-    }
-
-    override fun onBatchScanResults(results: List<ScanResult?>?) {
-    }
-
-    override fun onScanFailed(errorCode: Int) {
+        return false
     }
 }
