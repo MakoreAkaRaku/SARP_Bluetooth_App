1674c2480abf4412a6ed143cca45df10a2cf58af
MakoreAkaRaku <marcromancolom@gmail.com>
2025-02-12 11:46:59 +0100

    Advertise form now accepts SSID of 8 characters and password of 16 characters.

--- app/src/main/java/com/example/sarpapp/ui/components/WiFiLEAdvertiseFormView.kt
+++ app/src/main/java/com/example/sarpapp/ui/components/WiFiLEAdvertiseFormView.kt
@@ -1,5 +1,7 @@
 package com.example.sarpapp.ui.components
 
+import android.annotation.SuppressLint
+import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
@@ -24,6 +26,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
@@ -34,18 +37,26 @@ import androidx.compose.ui.unit.dp
 import com.example.sarpapp.BLUETOOTH_PERMISSIONS
 import com.example.sarpapp.BluetoothHandler
 
+@SuppressLint("MissingPermission")
 @Composable
 fun WiFiLEAdvertiseFormView(btHandler: BluetoothHandler) {
+    var context = LocalContext.current
     var ssidVal by rememberSaveable { mutableStateOf("") }
     var pwdVal by rememberSaveable { mutableStateOf("") }
     var pwdVisible by rememberSaveable { mutableStateOf(false) }
     val permissionLauncher =
         rememberLauncherForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
-
+            for (perm in it) {
+                if (!perm.value)
+                    return@rememberLauncherForActivityResult
+            }
+            btHandler.broadcastMsg(ssidVal, pwdVal)
         }
 
     Column(
-        modifier = Modifier.fillMaxWidth().padding(top = 20.dp, bottom = 20.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 20.dp, bottom = 20.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         OutlinedTextField(
@@ -55,14 +66,34 @@ fun WiFiLEAdvertiseFormView(btHandler: BluetoothHandler) {
             textStyle = TextStyle(fontWeight = FontWeight.Bold),
             maxLines = 1,
             singleLine = true,
-            onValueChange = { ssidVal = it },
+            onValueChange = {
+                if (it.length > BluetoothHandler.SSID_MAX_LENGTH) {
+                    Toast.makeText(
+                        context,
+                        "SSID too long, only up to ${BluetoothHandler.SSID_MAX_LENGTH} characters!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@OutlinedTextField
+                }
+                ssidVal = it
+            },
             modifier = Modifier.padding(20.dp),
             shape = RoundedCornerShape(40.dp)
         )
 
         OutlinedTextField(
             value = pwdVal,
-            onValueChange = { pwdVal = it },
+            onValueChange = {
+                if (it.length > BluetoothHandler.PWD_MAX_LENGTH) {
+                    Toast.makeText(
+                        context,
+                        "Password too long, only up to ${BluetoothHandler.PWD_MAX_LENGTH} characters!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@OutlinedTextField
+                }
+                pwdVal = it
+            },
             label = { Text("PASSWORD", textAlign = TextAlign.Center) },
             placeholder = { Text("Enter WiFi password") },
             singleLine = true,
@@ -72,7 +103,6 @@ fun WiFiLEAdvertiseFormView(btHandler: BluetoothHandler) {
                 val image = if (pwdVisible)
                     Icons.Filled.Visibility
                 else Icons.Filled.VisibilityOff
-                // TODO: make localized descriptions
                 val description = if (pwdVisible) "Hide password" else "Show password"
                 val iconTint = if (pwdVisible) Color.LightGray else Color.Gray
                 IconButton(onClick = { pwdVisible = !pwdVisible }) {
@@ -89,7 +119,7 @@ fun WiFiLEAdvertiseFormView(btHandler: BluetoothHandler) {
                 )
             },
             shape = RoundedCornerShape(10.dp),
-            modifier = Modifier.size(200.dp,50.dp)
+            modifier = Modifier.size(200.dp, 50.dp)
         ) {
             Text("Send Signal")
         }
